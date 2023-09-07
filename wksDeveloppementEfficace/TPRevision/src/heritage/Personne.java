63560f783c02deabf545be2cf3a531b1935605dc
FRANCOIS DE SAINT PALAIS <francois.desaintpala@iut.local>
2023-09-07 08:02:52 +0200

    Le workspace

---
+++ wksDeveloppementEfficace/TPRevision/src/heritage/Personne.java
@@ -0,0 +1,29 @@
+/*
+ * Presonne.java                                    6 sept. 2023
+ * IUT de Rodez, info1 2022-2023, aucun copyright ni copyleft
+ */
+
+package heritage;
+
+/** TODO comment class responsibility (SRP)
+ * @author francois.desaintpala
+ *
+ */
+public class Personne extends Individu {
+
+    private Telephone numero;
+    
+    private String addresseMail;
+
+    /** TODO comment initial state properties
+     * 
+     */
+    public Personne() {
+        super();
+        numero = new Telephone();
+    }
+    
+    public 
+    
+    
+}
